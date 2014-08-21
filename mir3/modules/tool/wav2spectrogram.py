8d6896bc8b2f28451058690a0f19fc99da7dfe5b
Tiago Tavares <tiagoft@gmail.com>
2014-08-21 19:12:57 -0300

    [initial_release] Added Hanning windowing to spectrogram

--- mir3/modules/tool/wav2spectrogram.py
+++ mir3/modules/tool/wav2spectrogram.py
@@ -22,10 +22,11 @@ class Wav2Spectrogram(mir3.module.Module):
         parser.add_argument('-l','--window-length', type=int, default=2048,
                             help="""window length, in samples (default:
                             %(default)s)""")
-        parser.add_argument('-S','--window-shape', default='Hanning',
-                            choices=['Bartlett', 'Blackman', 'Hamming',
-                            'Hanning', 'Triangle'], help="""shape of the window
-                            (default: %(default)s)""")
+        #parser.add_argument('-S','--window-shape', default='Hanning',
+        #                    choices=['Bartlett', 'Blackman', 'Hamming',
+        #                    'Hanning', 'Triangle'], help="""shape of the window
+        #                    (default: %(default)s)""")
+        # TODO: Enable window shapes again.
         parser.add_argument('-s','--window-step', type=int, default=1024,
                             help="""step, in samples, between windows (default:
                             %(default)s)""")
@@ -43,12 +44,12 @@ class Wav2Spectrogram(mir3.module.Module):
                          args.window_length,
                          args.dft_length,
                          args.window_step,
-                         args.window_shape,
+                         #args.window_shape,
                          args.spectrum_type)
         s.save(args.outfile)
 
     def convert(self, wav_file, window_length=2048, dft_length=2048,
-                window_step=1024, window_shape='Hanning',
+                window_step=1024,# window_shape='Hanning',
                 spectrum_type='magnitude', save_metadata=True):
         """Converts a WAV file to a spectrogram.
 
@@ -57,7 +58,7 @@ class Wav2Spectrogram(mir3.module.Module):
             window_length: window length for dft, in samples. Default: 2048.
             dft_length: dft length used. Default: 2048.
             window_step: step between windows, in samples. Default: 1024.
-            window_shape: shape for the filtered window. Default: 'Hanning'.
+            window_shape: shape for the filtered window (deprecated). Default: 'Hanning'.
             spectrum_type: type of spectrum extracted. Default: 'magnitude'.
             save_metadata: flag indicating whether the metadata should be
                            computed. Default: True.
@@ -70,7 +71,7 @@ class Wav2Spectrogram(mir3.module.Module):
         s.metadata.sampling_configuration.window_length = window_length
         s.metadata.sampling_configuration.dft_length    = dft_length
         s.metadata.sampling_configuration.window_step   = window_step
-        s.metadata.sampling_configuration.window_shape  = window_shape
+        #s.metadata.sampling_configuration.window_shape  = window_shape
         s.metadata.sampling_configuration.spectrum_type = spectrum_type
         if save_metadata:
             s.metadata.input = md.FileMetadata(wav_file)
@@ -91,11 +92,11 @@ class Wav2Spectrogram(mir3.module.Module):
                 
         if nSamples == 0:
             raise ValueError("File '%s' has no audio" % wav_file.name)
-
+        
         Pxx, freqs, bins, im = pylab.specgram(data,\
                         NFFT=s.metadata.sampling_configuration.window_length,\
                         Fs=s.metadata.sampling_configuration.fs,\
-                        #window=mlab.window_hanning,\
+                        window=pylab.window_hanning,\
                         noverlap=s.metadata.sampling_configuration.window_step-\
                         s.metadata.sampling_configuration.window_length,
                         pad_to=s.metadata.sampling_configuration.dft_length)
@@ -107,5 +108,7 @@ class Wav2Spectrogram(mir3.module.Module):
             Pxx = numpy.log10(numpy.sqrt(Pxx) + 10**(-6))
 
         s.data = Pxx
+
+        pylab.show()
         
         return s
