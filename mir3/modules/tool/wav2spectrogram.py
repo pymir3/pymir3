33119eb4f7415ded5cc42b75fc3325cccb360581
Tiago Tavares <tiagoft@gmail.com>
2014-08-21 18:23:33 -0300

    [initial_release] Removed Marsyas dependency

--- mir3/modules/tool/wav2spectrogram.py
+++ mir3/modules/tool/wav2spectrogram.py
@@ -1,11 +1,11 @@
 import argparse
 import numpy
+import scipy.io.wavfile
+import pylab
 
 import mir3.data.metadata as md
 import mir3.data.spectrogram as spectrogram
 import mir3.module
-
-#TODO: remove marsyas requirement
 
 class Wav2Spectrogram(mir3.module.Module):
     def get_help(self):
@@ -52,9 +52,6 @@ class Wav2Spectrogram(mir3.module.Module):
                 spectrum_type='magnitude', save_metadata=True):
         """Converts a WAV file to a spectrogram.
 
-        Currently we use Marsyas to do this stuff for us, so we accept any
-        window or spectrum available there.
-
         Args:
             wav_file: handler for an open wav file.
             window_length: window length for dft, in samples. Default: 2048.
@@ -78,77 +75,37 @@ class Wav2Spectrogram(mir3.module.Module):
         if save_metadata:
             s.metadata.input = md.FileMetadata(wav_file)
 
-        self.__convert_with_marsyas(s, wav_file)
+        self.__convert_native(s, wav_file)
 
         return s
 
-    def __convert_with_marsyas(self, s, wav_file):
-        s.metadata.method = md.Metadata(name='Marsyas')
-
-        # Initializes Marsyas
-        net, snet = self.__configure_marsyas(s)
-
-        # Configure the network to open the specified file
-        net.updControl(snet["src"]+"/mrs_string/filename", wav_file.name)
-
-        s.metadata.sampling_configuration.fs = \
-                net.getControl(snet["src"] + "/mrs_real/osrate").to_real()
+    def __convert_native(self, s, wav_file):
+        rate, data = scipy.io.wavfile.read(wav_file.name)
+        
+        s.metadata.sampling_configuration.fs = rate
         s.metadata.sampling_configuration.ofs = \
                 s.metadata.sampling_configuration.fs / \
                 s.metadata.sampling_configuration.window_step
 
-        # Sampling rate of the memory buffer
-        memFs = s.metadata.sampling_configuration.ofs
-        nSamples = \
-                net.getControl(snet["src"] + "/mrs_natural/size").to_natural()
-
+        nSamples = len(data)
+                
         if nSamples == 0:
             raise ValueError("File '%s' has no audio" % wav_file.name)
 
-        dur = nSamples / s.metadata.sampling_configuration.fs
-        memSize = int(dur * memFs)
-        DFT_Len = 1 + (s.metadata.sampling_configuration.dft_length/2)
+        Pxx, freqs, bins, im = pylab.specgram(data,\
+                        NFFT=s.metadata.sampling_configuration.window_length,\
+                        Fs=s.metadata.sampling_configuration.fs,\
+                        #window=mlab.window_hanning,\
+                        noverlap=s.metadata.sampling_configuration.window_step-\
+                        s.metadata.sampling_configuration.window_length,
+                        pad_to=s.metadata.sampling_configuration.dft_length)
 
-        s.data = numpy.zeros((DFT_Len, memSize))
-        for i in range(memSize):
-            net.tick()
-            # Gather results to a numpy array
-            s.data[:,i] = \
-                    net.getControl("mrs_realvec/processedData").to_realvec()
+        if s.metadata.sampling_configuration.spectrum_type == 'magnitude':
+            Pxx = numpy.sqrt(Pxx)
 
+        if s.metadata.sampling_configuration.spectrum_type == 'log':
+            Pxx = numpy.log10(numpy.sqrt(Pxx) + 10**(-6))
+
+        s.data = Pxx
+        
         return s
-
-    def __configure_marsyas(self, s):
-        """Internal method. Shouldn't be called directly.
-        """
-        import mir3.lib.marsyas_util as marsyas_util
-
-        # The memory was taken out of the network to improve the speed. The
-        # windows will be recorded otherwise.
-        spec_analyzer = ["Series/analysis", ["SoundFileSource/src",
-                                             "Sum/summation", "Gain/gain",
-                                             "ShiftInput/sft",
-                                             "Windowing/win", "Spectrum/spk",
-                                             "PowerSpectrum/pspk"]]
-        net = marsyas_util.create(spec_analyzer)
-        snet = marsyas_util.mar_refs(spec_analyzer)
-        configuration = s.metadata.sampling_configuration
-
-        net.updControl("mrs_natural/inSamples", configuration.window_step)
-        # This will un-normalize the DFT
-        net.updControl(snet["gain"]+"/mrs_real/gain",
-                       configuration.window_length*1.0)
-        net.updControl(snet["sft"]+"/mrs_natural/winSize",
-                       configuration.window_length)
-        net.updControl(snet["win"]+"/mrs_natural/zeroPadding",
-                       configuration.dft_length-configuration.window_length)
-        # "Hamming", "Hanning", "Triangle", "Bartlett", "Blackman"
-        net.updControl(snet["win"]+"/mrs_string/type",
-                       configuration.window_shape)
-        # "power", "magnitude", "decibels", "logmagnitude" (for
-        # 1+log(magnitude*1000), "logmagnitude2" (for 1+log10(magnitude)),
-        # "powerdensity"
-        net.updControl(snet["pspk"]+"/mrs_string/spectrumType",
-                       configuration.spectrum_type)
-
-        return net, snet
