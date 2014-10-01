3caf9f197deabde0e470f9450e53f98704e39dd1
Tiago Tavares <tiagoft@gmail.com>
2014-10-01 09:30:59 -0300

    [selfsimilarity] Added data container for self-similarity matrix

---
+++ mir3/data/self_similarity_matrix.py
@@ -0,0 +1,15 @@
+import mir3.data.data_object as do
+import mir3.data.metadata as md
+
+class SelfSimilarityMatrix(do.DataObject):
+    """Stores a feature matrix from a dataset.
+    """
+
+    def __init__(self):
+        super(FeatureMatrix, self).__init__(md.Metadata(feature=[],
+                        filename=[],
+                        sampling_configuration=md.Metadata(
+                            dft_length=1024, fs=None, ofs=None,
+                            spectrum_type='magnitude', window_length=1024,
+                            window_shape='Hanning', window_step=1024)))
+
