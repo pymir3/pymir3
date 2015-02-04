f486ab039e14818fd584ca8219f586ae8610a551
Tiago Tavares <tiagoft@gmail.com>
2015-02-04 12:11:59 -0200

    [structures] First naivebayes-based module for structure detection

---
+++ mir3/modules/structure/naivebayes.py
@@ -0,0 +1,46 @@
+import argparse
+import copy
+import numpy
+import scipy.stats
+import sys
+
+import mir3.data.feature_track as track
+import mir3.module
+import mir3.lib.naive_bayes as bayes
+
+class Texture(mir3.module.Module):
+    def get_help(self):
+        return """Searching structure using textures.."""
+
+    def build_arguments(self, parser):
+        parser.add_argument('infile', type=argparse.FileType('rb'),
+                            help="""input feature track""")
+
+        parser.add_argument('-s','--start', default=0, type=float,
+                            help="""start of desired structure (s) (default:
+                            %(default)s)""")
+        parser.add_argument('-e','--end', default=1, type=float,
+                            help="""end of desired structure (s) (default:
+                            %(default)s)""")
+
+    def run(self, args):
+        o = track.FeatureTrack()
+        o = o.load(args.infile)
+
+        std_o = o.data.std(axis=0)
+        o.data = (o.data - o.data.mean(axis=0))/\
+                    numpy.maximum(10**(-6), std_o)
+                
+        fs = o.metadata.sampling_configuration.ofs
+
+        # Estimating model
+        t0 = int(args.start * fs)
+        t1 = int(args.end * fs)
+        training_data = o.data[t0:t1,:]
+
+        # Applying model in a sliding window
+        output = numpy.array(bayes.naive_bayes
+                          (o.data, training_data))
+        for T0 in xrange(output.shape[0]):
+            print T0/float(fs), " ", output[T0]
+
