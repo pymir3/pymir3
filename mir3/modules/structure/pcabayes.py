0b3a1f0433951f8aeee55c11ea8e8d82ce9f2d48
Tiago Tavares <tiagoft@gmail.com>
2015-02-05 13:19:07 -0200

    [structures] Added PCA-Bayes module

---
+++ mir3/modules/structure/pcabayes.py
@@ -0,0 +1,52 @@
+import argparse
+import copy
+import numpy
+import scipy.stats
+import sys
+
+import mir3.data.feature_track as track
+import mir3.module
+import mir3.lib.naive_bayes as bayes
+import mir3.lib.median_filtering as median
+import mir3.lib.pca as pca
+
+class Texture(mir3.module.Module):
+    def get_help(self):
+        return """Searching structure using textures.."""
+
+    def build_arguments(self, parser):
+        parser.add_argument('infile', type=argparse.FileType('rb'),
+                            help="""input feature track""")
+
+        parser.add_argument('-s','--start', default=0, type=float,
+                            help="""start of desired structure (s) (default:
+                            %(default)s)""")
+        parser.add_argument('-e','--end', default=1, type=float,
+                            help="""end of desired structure (s) (default:
+                            %(default)s)""")
+
+    def run(self, args):
+        o = track.FeatureTrack()
+        o = o.load(args.infile)
+
+        std_o = o.data.std(axis=0)
+        o.data = (o.data - o.data.mean(axis=0))/\
+                    numpy.maximum(10**(-6), std_o)
+                
+        fs = o.metadata.sampling_configuration.ofs
+
+        # Estimating model
+        t0 = int(args.start * fs)
+        t1 = int(args.end * fs)
+        training_data = o.data[t0:t1,:]
+        testing_data = median.median_filter(o.data, t1-t0)
+        # PCA algorithm
+        orthogonal_testing, orthogonal_training = pca.subset_PCA \
+                          (testing_data, training_data)
+                
+        # Applying model in a sliding window
+        output = numpy.array(bayes.naive_bayes
+                          (orthogonal_testing, orthogonal_training))
+        for T0 in xrange(output.shape[0]):
+            print T0/float(fs), " ", output[T0]
+
