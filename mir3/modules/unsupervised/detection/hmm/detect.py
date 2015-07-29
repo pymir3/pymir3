f61064112b9faef5dc7620135849c5f0263c5580
Tiago <tiagoft@gmail.com>
2015-07-29 14:40:26 -0300

    [viterbi] Added submodule and test-script files for HMM detection

--- mir3/modules/unsupervised/detection/hmm/detect.py
+++ mir3/modules/unsupervised/detection/hmm/detect.py
@@ -1,8 +1,11 @@
 import argparse
+import numpy
 
 import mir3.data.linear_decomposition as ld
 import mir3.data.metadata as md
 import mir3.module
+import mir3.lib.ml.viterbi as vit
+
 
 class Detect(mir3.module.Module):
     """Binarizes a linear decomposition using a Hidden Markov Model.
@@ -50,15 +53,47 @@ class Detect(mir3.module.Module):
         else:
             metadata = None
 
-        meta = md.Metadata(name="threshold",
+        meta = md.Metadata(name="threshold-viterbi",
                            threshold=threshold,
                            activation_input=metadata,
                            original_method=None)
 
         # Binarizes the data and adjusts the metadata
         for k in d.data.right.keys():
-            d.data.right[k] = (d.data.right[k] >= threshold)
-            d.metadata.right[k] = md.Metadata(method="threshold",
+
+            # Separate high-value and low-value samples
+            (lin, col) = d.data.right[k].shape
+            pos_samples = []
+            neg_samples = []
+            for x in xrange(lin):
+                for y in xrange(col):
+                    if d.data.right[k][x,y] >= threhsold:
+                        pos_samples.append(d.data.right[k][x,y])
+                    else:
+                        neg_samples.append(d.data.right[k][x,y])
+
+            # Prepare for viterbi decoding
+            states = ('A', 'S')
+
+            start_probability = {'A': -9000, 'S': 0.0}
+            transition_probability = {
+                   'A' : {'A': 0.9, 'S': 0.1},
+                   'S' : {'A': 0.1, 'S': 0.9}
+                   }
+
+            emission_probability = {
+               'A' : [numpy.mean(pos_samples), numpy.var(pos_samples)],
+               'S' : [numpy.mean(neg_samples), numpy.var(neg_samples)]
+               }
+
+            # Viterbi decoding in each line of the activation matrix
+            for x in xrange(lin):
+                (p, o) = vit.viterbi(d.data.right[k][x,:], states,\
+                   start_probability, transition_probability,\
+                   emission_probability)
+                d.data.right[k][x,:] = numpy.array(o) == 'A'
+
+            d.metadata.right[k] = md.Metadata(method="threshold-viterbi",
                                               threshold=threshold,
                                               activation_input=metadata,
                                               original_method =
