e1ab2a216f035e48e0c4da28293df06829d9dc68
Tiago <tiagoft@gmail.com>
2015-08-07 09:21:39 -0300

    [unsupervised_nmf] Removed unused scripts

--- mir3/modules/unsupervised/linear/decomposer_submodule.py
+++
@@ -1,5 +0,0 @@
-import mir3.module
-
-class Decomposer(mir3.module.Module):
-    def get_help(self):
-        return """available linear decomposers of spectrograms"""
--- mir3/modules/unsupervised/linear_submodule.py
+++
@@ -1,5 +0,0 @@
-import mir3.module
-
-class Linear(mir3.module.Module):
-    def get_help(self):
-        return """unsupervised linear decompositions"""
