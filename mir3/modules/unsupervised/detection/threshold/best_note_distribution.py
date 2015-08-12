47c852ca66e67c00c2f54c2d37403c08c96f4763
Tiago <tiagoft@gmail.com>
2015-08-12 11:44:49 -0300

    [score_eval] Corrected script to use score-based features for threshold detection

---
+++ mir3/modules/unsupervised/detection/threshold/best_note_distribution.py
@@ -0,0 +1,49 @@
+import argparse
+import numpy
+
+import mir3.data.score as sc
+import mir3.data.metadata as md
+import mir3.lib.mir.midifeatures as feats
+import mir3.module
+
+
+class BestNoteDistribution(mir3.module.Module):
+    """Selects the score with the best note distribution
+    """
+
+    def get_help(self):
+        return """Finds the periodicity of a series of binarizations received as
+                    inputs"""
+
+    def build_arguments(self, parser):
+        parser.add_argument('-e', '--exponent', type=float, default=2.0,\
+                        help="""Exponent for the periodicity""")
+        parser.add_argument('infile', nargs='+',
+                        help="""score files""")
+
+    def run(self, args):
+        return self.find_best_note_distribution(args)
+
+    def find_best_note_distribution(self, args):
+        best_file_name = None
+        best_score = 9000000000
+        for filename in args.infile:
+            if best_file_name is None: # Avoids returning empty values
+                best_file_name = filename
+
+            with open(filename, 'rb') as handler:
+                a = sc.Score().load(handler)
+                event_list = feats.event_list(a.data)
+                if len(event_list) > 0: # Avoids empty scores
+                    (dens_mean, dens_std, dens_min, dens_max) = \
+                        feats.note_density(event_list)
+                    if dens_max < 10: # Avoids scores that are obviously wrong
+                        (max_range, mean_range, std_range) = \
+                            feats.relative_range(event_list)
+                        if max_range < best_score:
+                            best_score = max_range
+                            best_file_name = filename
+
+
+        print best_file_name
+
