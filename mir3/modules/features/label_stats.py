d2b5dccf13642f66ec572dcb70ab486ab9a46570
Tiago <tiagoft@gmail.com>
2016-01-18 09:47:37 -0200

    [classification] label_stats now work for yielding CSV information

--- mir3/modules/features/label_stats.py
+++ mir3/modules/features/label_stats.py
@@ -17,7 +17,7 @@ class Stats(mir3.module.Module):
         parser.add_argument('trackfile', type=argparse.FileType('rb'),
                             help="""input track file""")
         parser.add_argument('labelfile', type=str,
-                            help="""input track file""")
+                            help="""input label file""")
         parser.add_argument('outfile', type=argparse.FileType('wb'),
                             help="""output file""")
         parser.add_argument('-m','--mean', action='store_true', default=False,
@@ -43,7 +43,9 @@ class Stats(mir3.module.Module):
 
     def run(self, args):
         o = track.FeatureTrack()
-        o.load(args.trackfile)
+        o = o.load(args.trackfile)
+        #print o.metadata
+        #print o.data
 
         # Read label file
         onsets = []
@@ -57,80 +59,21 @@ class Stats(mir3.module.Module):
                 offsets.append(float(L[1]))
                 L[2].replace('-', '+')
                 labels.append(str(L[2].split('+')[0]))
-                print onsets[-1], offsets[-1], labels[-1]
-        exit()
+                #print onsets[-1], offsets[-1], labels[-1]
+        #exit()
 
         final_output = None
         final_filenames = []
+        final_filenames.append(o.metadata.filename)
+        if o.data.ndim == 1:
+            o.data.shape = (o.data.size, 1)
+        feats = o.metadata.feature.split()
+        a = numpy.array(feats)
+        i = a.argsort()
+        ofs = o.metadata.sampling_configuration.ofs
+        #print offsets[-1], o.data.shape, o.data.shape[0] / float(ofs)
+        #print i
 
-        for a in args.infiles:
-            o = o.load(a)
-
-            #print o.metadata.feature
-
-            a = numpy.array(o.metadata.feature.split())
-            i = a.argsort()
-
-            #print a
-            #print o.data.mean(axis=0)
-
-            #print i
-            #print a[i]
-            #print o.data.mean(axis=0)[i], "\n"
-
- #           print o.data.shape
-
-            final_filenames.append(o.metadata.filename)
-            if o.data.ndim == 1:
-                o.data.shape = (o.data.size, 1)
-
-            out = numpy.array([])
-            if args.mean is True:
-                out = numpy.hstack((out,
-                             o.data.mean(axis=0)[i]))
-
-#            print out.shape
-
-            if args.variance is True:
-                out = numpy.hstack((out,
-                             o.data.var(axis=0)[i]))
-
-#            print out.shape
-            if args.slope is True:
-                variance = o.data.var(axis=0)[i]
-                lindata = numpy.zeros(variance.shape)
-                for i in xrange(o.data.shape[1]):
-                    lindata[i] = scipy.stats.linregress(o.data[:,i],\
-                                            range(o.data.shape[0]))[0]
-
-                out = numpy.hstack((out,lindata))
-
-            if args.limits is True:
-                out = numpy.hstack((out,
-                             o.data.max(axis=0)[i]))
-                out = numpy.hstack((out,
-                             o.data.argmax(axis=0)[i]/float(o.data.shape[0])))
-                out = numpy.hstack((out,
-                             o.data.min(axis=0)[i]))
-                out = numpy.hstack((out,
-                             o.data.argmin(axis=0)[i]/float(o.data.shape[0])))
-
-
-                out.shape = (1, out.size)
-
-            if args.csv is True:
-                for i in xrange(len(out)-1):
-                    sys.stdout.write(str(out[i]))
-                    sys.stdout.write(",")
-                sys.stdout.write(str(out[-1]))
-                sys.stdout.write('\n')
-
-            if final_output is None:
-                final_output = out
-            else:
-                final_output = numpy.vstack( (final_output, out) )
-
-        # Dealing with feature metadata:
         my_features = o.metadata.feature.split()
         my_features.sort()
         new_features = ""
@@ -152,6 +95,55 @@ class Stats(mir3.module.Module):
                 new_features = new_features + " " + "min_" + feat
             for feat in my_features:
                 new_features = new_features + " " + "argmin_" + feat
+
+        new_features = new_features.strip()
+
+        if args.csv is True:
+            sys.stdout.write(new_features.replace(' ', ','))
+            sys.stdout.write(',LABEL')
+            sys.stdout.write('\n')
+
+        #exit()
+
+        for d in xrange(len(onsets)):
+            out = numpy.array([])
+            i = a.argsort()
+
+            minN = int(onsets[d] * float(ofs))
+            maxN = int(offsets[d] * float(ofs))
+            if maxN <= (minN+1):
+                maxN = minN + 2
+            #print minN, maxN, onsets[d], offsets[d], o.data.shape[0], ofs
+
+            if args.mean is True:
+                out = numpy.hstack((out,
+                    o.data[minN:maxN,:].mean(axis=0)[i]))
+                #print out.shape, o.data[minN:maxN,:].mean(axis=0).shape
+
+            if args.variance is True:
+                out = numpy.hstack((out,
+                    o.data[minN:maxN,:].var(axis=0)[i]))
+
+            if args.slope is True:
+                variance = o.data[minN:maxN,:].var(axis=0)[i]
+                lindata = numpy.zeros(variance.shape)
+                for i in xrange(o.data[minN:maxN,i].shape[1]):
+                    lindata[i] = scipy.stats.linregress(o.data[minN:maxN,i],\
+                                     range(o.data[minN:maxN,:].shape[0]))[0]
+
+                out = numpy.hstack((out,lindata))
+
+            if args.csv is True:
+                for i in xrange(len(out)):
+                    sys.stdout.write(str(out[i]))
+                    sys.stdout.write(",")
+                sys.stdout.write(labels[d])
+                sys.stdout.write('\n')
+
+            if final_output is None:
+                final_output = out
+            else:
+                final_output = numpy.vstack( (final_output, out) )
 
         p = feature_matrix.FeatureMatrix()
         p.data = final_output.copy()
