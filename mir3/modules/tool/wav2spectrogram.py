61a7dfadb89869c7d58f621901c894fbcfb84634
Juliano Henrique Foleiss <julianofoleiss@utfpr.edu.br>
2016-03-16 22:29:41 -0300

    Added load_wav function capable of reading 24-bit wav files.
    This function also resamples wav data and mixes into mono if needed.

--- mir3/modules/tool/wav2spectrogram.py
+++ mir3/modules/tool/wav2spectrogram.py
@@ -2,7 +2,9 @@ import argparse
 import numpy
 import copy
 import scipy.io.wavfile
-
+import scipy.signal
+import wave
+import os
 
 import mir3.data.metadata as md
 import mir3.data.spectrogram as spectrogram
@@ -48,6 +50,80 @@ class Wav2Spectrogram(mir3.module.Module):
                          args.spectrum_type)
         s.save(args.outfile)
 
+    def load_audio(self, filename, mono=True, fs=44100):
+        """Load audio file into numpy array
+        Supports 24-bit wav-format, and flac audio through librosa.
+        Parameters
+        ----------
+        filename:  str
+            Path to audio file
+        mono : bool
+            In case of multi-channel audio, channels are averaged into single channel.
+            (Default value=True)
+        fs : int > 0 [scalar]
+            Target sample rate, if input audio does not fulfil this, audio is resampled.
+            (Default value=44100)
+        Returns
+        -------
+        audio_data : numpy.ndarray [shape=(signal_length, channel)]
+            Audio
+        sample_rate : integer
+            Sample rate
+
+        from: https://github.com/TUT-ARG/DCASE2016-baseline-system-python/blob/master/src/files.py
+        """
+
+        #I should include this check back at some point...
+        # file_base, file_extension = os.path.splitext(filename)
+        # if file_extension == '.wav':
+        audio_file = wave.open(filename)
+
+        # Audio info
+        sample_rate = audio_file.getframerate()
+        sample_width = audio_file.getsampwidth()
+        number_of_channels = audio_file.getnchannels()
+        number_of_frames = audio_file.getnframes()
+
+        # Read raw bytes
+        data = audio_file.readframes(number_of_frames)
+        audio_file.close()
+
+        # Convert bytes based on sample_width
+        num_samples, remainder = divmod(len(data), sample_width * number_of_channels)
+        if remainder > 0:
+            raise ValueError('The length of data is not a multiple of sample size * number of channels.')
+        if sample_width > 4:
+            raise ValueError('Sample size cannot be bigger than 4 bytes.')
+
+        if sample_width == 3:
+            # 24 bit audio
+            a = numpy.empty((num_samples, number_of_channels, 4), dtype=numpy.uint8)
+            raw_bytes = numpy.fromstring(data, dtype=numpy.uint8)
+            a[:, :, :sample_width] = raw_bytes.reshape(-1, number_of_channels, sample_width)
+            a[:, :, sample_width:] = (a[:, :, sample_width - 1:sample_width] >> 7) * 255
+            audio_data = a.view('<i4').reshape(a.shape[:-1]).T
+        else:
+            # 8 bit samples are stored as unsigned ints; others as signed ints.
+            dt_char = 'u' if sample_width == 1 else 'i'
+            a = numpy.fromstring(data, dtype='<%s%d' % (dt_char, sample_width))
+            audio_data = a.reshape(-1, number_of_channels).T
+
+        if mono:
+            # Down-mix audio
+            audio_data = numpy.mean(audio_data, axis=0)
+
+        # Convert int values into float
+        audio_data = audio_data / float(2 ** (sample_width * 8 - 1) + 1)
+
+        # Resample
+        if fs != sample_rate:
+            audio_data = scipy.signal.resample(audio_data, len(audio_data) * (float(fs) / sample_rate) )
+            sample_rate = fs
+
+        return sample_rate, audio_data
+
+        #return None, None
+
     def convert(self, wav_file, window_length=2048, dft_length=2048,
                 window_step=1024,
                 spectrum_type='magnitude', save_metadata=True):
@@ -77,9 +153,10 @@ class Wav2Spectrogram(mir3.module.Module):
             s.metadata.input = md.FileMetadata(wav_file)
 
         # Calculates data
-        rate, data = scipy.io.wavfile.read(wav_file.name)
+        #rate, data = scipy.io.wavfile.read(wav_file.name)
+        #data = data.astype(numpy.float)
 
-        data = data.astype(numpy.float)
+        rate, data = self.load_audio(wav_file)
 
         #data /= numpy.max(numpy.abs(data)) # Normalization to -1/+1 range
 
