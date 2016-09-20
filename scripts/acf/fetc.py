4bec86d55ffe1ae61a2b9e6877597be457fffb2f
Juliano Henrique Foleiss <julianofoleiss@utfpr.edu.br>
2016-09-20 14:03:40 -0300

    improved command line support.

--- scripts/acf/fetc.py
+++ scripts/acf/fetc.py
@@ -26,7 +26,13 @@ def update_parameters(params):
         params['model_testing']['predict_proba_file'] = ttag + "." + params['model_testing']['predict_proba_file']
 
 def read_parameters(param_file):
-    with open(param_file, 'r') as f:
+    try:
+        f = open(param_file, 'r')
+    except IOError:
+        print "Could not read file:", param_file
+        sys.exit()
+
+    with f:
         params = yaml.load(f)
 
     return params
@@ -48,6 +54,14 @@ def parse_commandline(argv):
         ov.append(("general.feature_extraction_filelist", argv[ex_pos+2]))
         return ov
 
+    def switch_aggregate(argv):
+        ov = [("steps.extract_features", False),
+              ("steps.aggregate_features", True),
+              ("steps.train", False),
+              ("steps.test", False),
+              ("steps.evaluate", False)]
+        return ov
+
     def switch_train(argv):
         ov = [("steps.extract_features", False),
                 ("steps.aggregate_features", False),
@@ -62,6 +76,23 @@ def parse_commandline(argv):
             exit(1)
         ov.append(("general.scratch_directory", argv[ex_pos+1]))
         ov.append(("general.train_filelist", argv[ex_pos+2]))
+
+        return ov
+
+    def switch_evaluate(argv):
+        ov = [("steps.extract_features", False),
+              ("steps.aggregate_features", False),
+              ("steps.train", False),
+              ("steps.test", False),
+              ("steps.evaluate", True)]
+
+        ex_pos = argv.index('-evaluate')
+        if len(argv) - ex_pos < 3:
+            print "wrong number of arguments for \'evaluate\'. usage: %s -evaluate path_to_predict_file path_to_label_file" % \
+                  (argv[0])
+            exit(1)
+        ov.append(("general.predict_file", argv[ex_pos + 1]))
+        ov.append(("general.label_file", argv[ex_pos + 2]))
 
         return ov
 
@@ -81,18 +112,35 @@ def parse_commandline(argv):
         ov.append(("general.scratch_directory", argv[ex_pos + 1]))
         ov.append(("general.test_filelist", argv[ex_pos + 2]))
         ov.append(("general.predict_file", argv[ex_pos + 3]))
+
         return ov
+
+
+    def set_int(argv):
+        ex_pos = argv.index('-si')
+        a = argv[ex_pos+1]
+        a = a.split("=")
+        return (a[0], int(a[1]))
 
     ovw = []
 
     if "-extract" in argv:
         ovw.extend(switch_extract(argv))
 
+    elif "-aggregate" in argv:
+        ovw.extend(switch_aggregate(argv))
+
     elif "-train" in argv:
         ovw.extend(switch_train(argv))
 
     elif "-test" in argv:
         ovw.extend(switch_test(argv))
+
+    elif "-evaluate" in argv:
+        ovw.extend(switch_evaluate(argv))
+
+    elif "-si" in argv:
+        ovw.extend(set_int(argv))
 
     return ovw
 
@@ -117,7 +165,13 @@ def overwrite_params(params, ov):
 
 def run_fetc():
 
-    exp = read_parameters(param_file="experiment.yaml")
+    if "-exp" in sys.argv:
+        exp_pos = sys.argv.index("-exp")
+        param_file = sys.argv[exp_pos+1]
+    else:
+        param_file = "experiment.yaml"
+
+    exp = read_parameters(param_file=param_file)
 
     ovw = parse_commandline(sys.argv)
     overwrite_params(exp, ovw)
