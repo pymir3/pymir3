54b8ee76f6929933717b25318a7ff526b21e1e88
Juliano Henrique Foleiss <julianofoleiss@utfpr.edu.br>
2016-10-28 17:16:09 -0200

    added debug capabilities to parallel feature extr.

--- scripts/acf/fe_bandwise.py
+++ scripts/acf/fe_bandwise.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import time
+import traceback
 
 sys.path.append("../../")
 sys.path.append("../dcase2016/")
@@ -49,46 +50,53 @@ class BandwiseExtractor(FeatureExtractor):
                 * ['general']['dft']['window_step']
 
         """
-        scale = self.params['bandwise_extraction']['scale']
-        num_bands = self.params['bandwise_extraction']['number_of_bands']
-        also_one_band = self.params['bandwise_extraction']['also_one_band']
-        discard_bin_zero = self.params['bandwise_extraction']['discard_bin_zero']
-        dft_len = self.params['general']['dft']['len']
-        dft_window_size = self.params['general']['dft']['window_size']
-        dft_window_step = self.params['general']['dft']['window_step']
-        desired_sample_rate = self.params['general']['sample_rate']
-        to_mono = self.params['general']['mono']
-        zero_pad_resampling = self.params['general']['zero_pad_resampling']
 
-        T0 = time.time()
-        logger.debug("Extracting features for %s", filename)
+        try:
+            
+            scale = self.params['bandwise_extraction']['scale']
+            num_bands = self.params['bandwise_extraction']['number_of_bands']
+            also_one_band = self.params['bandwise_extraction']['also_one_band']
+            discard_bin_zero = self.params['bandwise_extraction']['discard_bin_zero']
+            dft_len = self.params['general']['dft']['len']
+            dft_window_size = self.params['general']['dft']['window_size']
+            dft_window_step = self.params['general']['dft']['window_step']
+            desired_sample_rate = self.params['general']['sample_rate']
+            to_mono = self.params['general']['mono']
+            zero_pad_resampling = self.params['general']['zero_pad_resampling']
 
-        #todo: handle file not found errors!
+            T0 = time.time()
+            logger.debug("Extracting features for %s", filename)
 
-        feats = BF.BandwiseFeatures(filename,
-                                    dft_len=dft_len,
-                                    window_len=dft_window_size,
-                                    window_step=dft_window_step,
-                                    fs=desired_sample_rate,
-                                    mono=to_mono,
-                                    zero_pad_resampling=zero_pad_resampling)
+            #todo: handle file not found errors!
 
-        if scale == 'one':
-            a = BF.OneBand(low=int(feats.spectrogram.metadata.min_freq),
-                           high=int(feats.spectrogram.metadata.max_freq))
+            feats = BF.BandwiseFeatures(filename,
+                                        dft_len=dft_len,
+                                        window_len=dft_window_size,
+                                        window_step=dft_window_step,
+                                        fs=desired_sample_rate,
+                                        mono=to_mono,
+                                        zero_pad_resampling=zero_pad_resampling)
 
-        if scale == 'linear':
-            a = BF.LinearBand(low=int(feats.spectrogram.metadata.min_freq),
-                              high=int(feats.spectrogram.metadata.max_freq),
-                              nbands=num_bands)
-        if scale == 'mel':
-            a = BF.MelBand(low=int(feats.spectrogram.metadata.min_freq),
-                           high=int(feats.spectrogram.metadata.max_freq),
-                           nbands=num_bands)
+            if scale == 'one':
+                a = BF.OneBand(low=int(feats.spectrogram.metadata.min_freq),
+                            high=int(feats.spectrogram.metadata.max_freq))
 
-        feats.calculate_features_per_band(a, also_one_band=also_one_band, discard_bin_zero=discard_bin_zero)
-        T1 = time.time()
-        logger.debug("Feature extraction took %f seconds", T1 - T0)
+            if scale == 'linear':
+                a = BF.LinearBand(low=int(feats.spectrogram.metadata.min_freq),
+                                high=int(feats.spectrogram.metadata.max_freq),
+                                nbands=num_bands)
+            if scale == 'mel':
+                a = BF.MelBand(low=int(feats.spectrogram.metadata.min_freq),
+                            high=int(feats.spectrogram.metadata.max_freq),
+                            nbands=num_bands)
 
-        feats.join_bands(crop=True)
-        return feats.joined_features
+            feats.calculate_features_per_band(a, also_one_band=also_one_band, discard_bin_zero=discard_bin_zero)
+            T1 = time.time()
+            logger.debug("Feature extraction took %f seconds", T1 - T0)
+
+            feats.join_bands(crop=True)
+            return feats.joined_features
+
+        except Exception:
+            traceback.print_exc()
+            raise
