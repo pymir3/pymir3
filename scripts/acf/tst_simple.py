eda4e5cc2e0b15a4c6653f49ee6df4bc3087be31
Juliano Henrique Foleiss <julianofoleiss@utfpr.edu.br>
2016-10-28 16:59:57 -0200

    fixed confusion matrix printing...

--- scripts/acf/tst_simple.py
+++ scripts/acf/tst_simple.py
@@ -4,66 +4,6 @@ import time
 import numpy
 import dill
 
-from sklearn.metrics import confusion_matrix
-
-def print_cm(cm, labels, hide_labels=True, file=None, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
-    """pretty print for confusion matrixes"""
-    #TODO: implementar hide_labels para saida em arquivo
-    columnwidth = max([len(x) for x in labels]+[0]) # 5 is value length
-
-    if hide_labels:
-        columnwidth = 3
-
-    empty_cell = " " * columnwidth
-    # Print header
-    if file is None:
-        print "    " + empty_cell,
-    else:
-        file.write("    " + empty_cell)
-    i = 0
-    for label in labels:
-        if file is None:
-            if hide_labels:
-                print "%{0}d".format(columnwidth) % i,
-            else:
-                print "%{0}s".format(columnwidth) % label,
-        else:
-            file.write("%{0}s".format(columnwidth) % label)
-        i+=1
-    if file is None:
-        print
-    else:
-        file.write("\n")
-    # Print rows
-    for i, label1 in enumerate(labels):
-        if file is None:
-            if hide_labels:
-                print "    %{0}d".format(columnwidth) % i,
-            else:
-                print "    %{0}s".format(columnwidth) % label1,
-        else:
-            file.write("    %{0}s".format(columnwidth) % label1)
-        for j in range(len(labels)):
-            cell = "%{0}d".format(columnwidth) % cm[i, j]
-            if hide_zeroes:
-                cell = cell if float(cm[i, j]) != 0 else empty_cell
-            if hide_diagonal:
-                cell = cell if i != j else empty_cell
-            if hide_threshold:
-                cell = cell if cm[i, j] > hide_threshold else empty_cell
-            if file is None:
-                print cell,
-            else:
-                file.write(cell)
-        if file is None:
-            print
-        else:
-            file.write("\n")
-
-    for i, l in enumerate(labels):
-        print "%d: %s" % (i, l)
-
-
 class SimpleModelTester(ModelTester):
 
     def __init__(self):
@@ -83,12 +23,6 @@ class SimpleModelTester(ModelTester):
         features = scaler.transform(test_data.features)
 
         predicted = model.predict(features)
-
-        print("Confusion matrix:")
-        
-        cm = confusion_matrix(input.truth, input.predicted)
-
-        print_cm(cm, model.classes_)
 
         #output predict file
         predict_filename = self.params['general']['predict_file']
